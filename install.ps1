3ddc0ff04c4e230fbe54c063dc0ab9f19ed2fb66
Adnan Gonzaga Ioricce <adnanioricce@outlook.com>
2022-08-06 17:24:13 -0300

    adding powershell script for use on windows

---
+++ install.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install neovim -y
+cp init.vim $env:LOCALAPPDATA\nvim\
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+Set-Alias vi C:\tools\neovim\nvim-win64\bin\nvim.exe
+Set-Alias vim C:\tools\neovim\nvim-win64\bin\nvim.exe
